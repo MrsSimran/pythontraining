03d9affa851e6df13eb3b282984954f4fcda2093
simran <simranshiekh41@gmail.com>
2024-12-24 16:03:27 +0530

    arthematic data

---
+++ Day2/arthmetic.py
@@ -0,0 +1,24 @@
+a=40
+b=10
+print(a+b,"add") # add 50
+print(a-b,"minus")#minus 30
+print(a*b,"multiple")# multiple 400
+print(a**b,"square root")# square root
+print(a/b,"divide")#0.66666666# print(a//b,"quotient") # quotient
+print(a%b,"remider")# remider 40
+print(a==b,"boolean")#false
+
+# c=5
+# d=10
+# print(c>d,"grater than")#grater than
+# print(c<d,"less than")
+# print(c==d,"equal to")#false
+# print(c!=d," not equal to")
+# print(c<=d,"less than")# 2 condition ,a less than and equal to b
+# print(c>=d,"greater than")# 2 condition ,a greater than and equal to b
+
+T=True
+F=False
+print(T and F ,"<--And operator")
+print(T or F,"<--or operator")
+print(not F,"<--not operate")
