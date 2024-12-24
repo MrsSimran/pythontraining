4eb590f32e8dedeb035dad9b4ef73fb032103aa7
simran <simranshiekh41@gmail.com>
2024-12-24 16:03:27 +0530

    deleting variable

---
+++ Day2/deletingvar.py
@@ -0,0 +1,12 @@
+# a=12
+# b=55
+# print(a,b)
+# del a
+# print(b)
+# print(a)#since a variable is deleted "NameError: name 'a' is not defined"
+# # if we reverse the print satement then aslo error will come due to variable got delete
+
+a=12
+b=55
+del a
+print(a,b)
