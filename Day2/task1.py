e033696ada03200e513adbe50f293abc2350e9a3
Simran <simranshiekh41@gmail.com>
2025-06-12 01:25:57 +0530

    basic input& output task

---
+++ Day2/task1.py
@@ -0,0 +1,10 @@
+# Write a program that:
+# Takes your name and age
+# Prints a greeting like Hello Simran, you are 24 years old.
+# Create variables of each type: int, float, str, bool, list — and print their types
+# Take 2 numbers from user, add them and print result  
+
+name= str(input( "enter name:"))
+age= int(input("enter age"))
+print("hello :{} ,your age is: {}".format(name,age))
+
